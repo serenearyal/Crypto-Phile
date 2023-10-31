6a1d72a14bdede35cb2ef7681f409ea3c7930731
Serene <serene.aryal24@gmail.com>
2023-10-31 14:04:13 -0500

    icon

--- src/components/market/Coin.jsx
+++ src/components/market/Coin.jsx
@@ -5,6 +5,7 @@ import "./coin.css"
 const Coin = ({coin}) => {
   return (
     <Link className='coin' to={`coin/${coin.id}`}>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <div className='name-logo'>
             <img src={coin.image} alt="" />
             <p>{coin.name} <span>({coin.symbol})</span></p>
